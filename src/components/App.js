6867ad62beb08de6d6beaa702b5e0c88ce9ddf03
Taylor Huffman <taylor@crucialartists.com>
2022-03-16 10:24:58 -0500

    Pass 20 out of 22 tests

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,62 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Filters from "./Filters";
 import PetBrowser from "./PetBrowser";
 
+const API = 'http://localhost:3001/pets'
+
 function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
+
+  useEffect(() => {
+    fetch(API)
+    .then(r => r.json())
+    .then(pets => setPets(pets))
+  }, [])
+
+  function onChangeType(filterType) {
+    setFilters({type: filterType})
+  }
+
+  function onFindPetsClick() {
+    if (filters.type !== 'all') {
+      fetch(`${API}?type=${filters.type}`)
+      .then(r => r.json())
+      .then(pets => setPets(pets))
+    } else {
+      fetch(API)
+      .then(r => r.json())
+      .then(pets => setPets(pets))
+    }
+  }
+
+  function onAdoptPet(id) {
+    // console.log(id)
+    // const petsMinusAdopted = pets.filter(pet => pet.id !== id)
+    // foundPet.isAdopted = true
+    // console.log(foundPet)
+
+    fetch(`${API}/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        isAdopted: true
+      })
+    })
+    .then(r => r.json())
+    // .then(() => fetch(API)
+    // .then(r => r.json())
+    // .then(pets => setPets(pets)))
+  }
+
+  // function onAdoptPet() {
+  //   fetch(API)
+  //   .then(r => r.json())
+  //   .then(pets => setPets(pets))
+  // }
 
   return (
     <div className="ui container">
@@ -15,10 +66,10 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters onChangeType={onChangeType} onFindPetsClick={onFindPetsClick} />
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} api={API} onAdoptPet={onAdoptPet} />
           </div>
         </div>
       </div>
--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,24 +1,47 @@
 import React from "react";
 
-function Pet() {
+function Pet({ pet, api, onAdoptPet }) {
+
+  const { id, type, gender, age, weight, name, isAdopted } = pet
+  
+
+  // function handleOnClick() {
+  //   fetch(`${api}/${id}`, {
+  //     method: 'PATCH',
+  //     headers: {
+  //       'Content-type': 'application/json',
+  //     },
+  //     body: JSON.stringify({
+  //       isAdopted: true
+  //     })
+  //   })
+  //   .then(r => r.json())
+  //   .then(() => onAdoptPet())
+  // }
+
+
+  function handleOnClick() {
+    onAdoptPet(id)
+  }
+
   return (
     <div className="card" data-testid="pet">
       <div className="content">
         <span className="header">
-          {/*'♀' OR '♂' */}
-          PET NAME
+          {gender === 'male' ? '♂ ' : '♀ '}
+          {name}
         </span>
         <div className="meta">
-          <span className="date">PET TYPE</span>
+          <span className="date">{type}</span>
         </div>
         <div className="description">
-          <p>Age: PET AGE</p>
-          <p>Weight: PET WEIGHT</p>
+          <p>Age: {age}</p>
+          <p>Weight: {weight}</p>
         </div>
       </div>
       <div className="extra content">
-        <button className="ui disabled button">Already adopted</button>
-        <button className="ui primary button">Adopt pet</button>
+        {isAdopted ? <button className={isAdopted ? "ui button" : "ui disabled button"}>Already adopted</button> : null}
+        {isAdopted ? null : <button onClick={handleOnClick} className={isAdopted ? "ui disabled primary button" : "ui primary button"}>Adopt pet</button>}
       </div>
     </div>
   );
